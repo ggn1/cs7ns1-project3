309b84f39254a999125a7f1a5d7c2580224d35b4
TarunSingh44 <48924067+TarunSingh44@users.noreply.github.com>
2023-11-26 18:55:27 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# AUTHOR [START]: Gayathri Girish Nair (23340334) - Tarun Singh (23330140) - Tejas Bhatnagar (23334930)
 #!/bin/bash
-
 python3 runme.py
+# AUTHOR [START]: Gayathri Girish Nair (23340334) - Tarun Singh (23330140) - Tejas Bhatnagar (23334930)
